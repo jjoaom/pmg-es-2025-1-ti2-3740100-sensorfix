a7fe7f9c905ba2287668884268c911815898505c
jjoaom <joaomarcos.aquino@hotmail.com>
2025-05-16 15:25:00 -0300

    add: pagina de login && started producao backend

---
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/config/AppSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.luizgustavo.sensor_fix.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    // Getters (se quiser usar em outro lugar)
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Você também pode usar um método @PostConstruct para testar, tipo:
+    // @PostConstruct
+    // public void init() {
+    //     System.out.println("Usuário: " + username + ", Senha: " + password);
+    // }
+}
---
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/controller/Demandas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.luizgustavo.sensor_fix.controller;
+
+/**
+ *
+ * @author joaom
+ */
+public class Demandas {
+    
+}
