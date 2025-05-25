59a71fa5409a08a664ddfd711e1ca57d5c5945a1
jjoaom <joaomarcos.aquino@hotmail.com>
2025-05-25 19:44:39 -0300

    add: adicionado novas logicas ao backend de producao

--- src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/models/Peca.java
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/models/Peca.java
@@ -1,17 +1,27 @@
 package com.luizgustavo.sensor_fix.models;
 
-import jakarta.persistence.*;
-/**
- *
- * @author joaom
- */
+import java.util.Objects;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Entity
+@Table(name = Peca.TABLE_NAME)
 public class Peca {
+    public static final String TABLE_NAME = "peca";
+
+    public interface CreatePeca{
+
+    }
+    public interface UpdatePeca{
+
+    }
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     @Column(name = "id", unique = true)
-    private long id;
+    private Long id;
     
     @Column(name = "nome", nullable = false, length = 100, unique = true)
     @NotNull
@@ -26,4 +36,61 @@ public class Peca {
     @Column(name = "quantidade", nullable = false)
     private int qtd;
     
+    public Peca(){
+
+    }
+    public Peca(Long id, String nome, String descricao, int qtd){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.qtd = qtd;
+    }
+    
+    public Long getId(){
+        return this.id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public int getQtd(){
+        return this.qtd;
+    }
+    public void setQtd(int qtd){
+        this.qtd = qtd;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Peca)) return false;
+        Peca that = (Peca) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Peça{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", Descrição=" + descricao + + '\'' +
+                ", quantidade=" + qtd + '\'' +
+                '}';
+    }
 }
---
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/services/PecaService.java
@@ -0,0 +1,64 @@
+package main.java.com.luizgustavo.sensor_fix.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luizgustavo.sensor_fix.models.Peca;
+import com.luizgustavo.sensor_fix.repositories.PecaRepository;
+
+@Service
+public class PecaService {
+    @Autowired
+    private PecaRepository pecaRepository;    
+
+    // Buscar Peça por ID
+    public Peca findById(Long id){
+        Optional<Peca> peca = this.pecaRepository.findById(id);
+        return peca.orElseThrow(() ->
+            new RuntimeErrorException("Peça não encontrada ou não existente. ID: " + id + ", Tipo: " + Peca.class.getName()));
+    }
+    // Buscar Peca por Nome
+    public Peca findByName(String name){
+        Optional<Peca> peca = this.pecaRepository.findByName(name);
+        return peca.orElseThrow(() ->
+            new RuntimeErrorException("Peça não encontrada ou não existente. Nome: " + name + ", Tipo: " + Peca.class.getName()));
+    }
+    //Criar nova Peça
+    @Transactional
+    public Peca create(Peca obj){
+        obj.setId(null);
+        obj = this.pecaRepository.save(obj);
+        return obj;
+    }
+    //Atualizar peça existente
+    @Transactional
+    public Peca update(Peca obj){
+        Peca newObj = findById(obj.getId());
+
+        newObj.setNome(obj.getNome());
+        newObj.setDescricao(obj.getDescricao());
+        newObj.setQtd(obj.getQtd());
+        return this.pecaRepository.save(newObj);
+    }
+    //Excluir Peça
+    public void delete(Long id){
+        findById(id);
+
+        try{
+            this.pecaRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas.")
+        }
+    }
+
+    //Buscar todas as peças
+    public List<Peca> findAll(){
+        return this.pecaRepository.findAll();
+    }
+}
