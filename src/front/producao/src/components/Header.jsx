794cb63ce11dd8832085f611d5c33aa083dd7b23
jjoaom <joaomarcos.aquino@hotmail.com>
2025-05-30 15:31:57 -0300

    edit: new css for header

--- src/front/producao/src/components/Header.jsx
+++ src/front/producao/src/components/Header.jsx
@@ -7,17 +7,16 @@ import { IoLogOutOutline } from "react-icons/io5";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg header-div">
+    <nav className="navbar navbar-expand-lg bg-primary py-2">
       <div className="container-fluid">
-        <div className="navbar-brand">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
           <img
             src="./logo1.png"
             alt="Logo"
-            width="100%"
-            height="64"
-            className="d-inline-block align-text-top"
+            style={{ height: "clamp(40px, 5vw, 56px)" }}
+            className="d-inline-block align-text-top me-2 img-fluid"
           />
-        </div>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -31,34 +30,38 @@ export default function Header() {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 flex-column flex-lg-row align-items-start align-items-lg-center">
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/">
-                <FaHome /> Home
+              <Link className="nav-link text-white d-flex align-items-center gap-1 py-2" to="/">
+                <FaHome /> <span className="">Home</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/producao">
-                <MdPrecisionManufacturing /> Produção
+              <Link className="nav-link text-white d-flex align-items-center gap-1 py-2" to="/producao">
+                <MdPrecisionManufacturing /> <span className="">Produção</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/estoque">
-                <BsBoxSeam /> Estoque
+              <Link className="nav-link text-white d-flex align-items-center gap-1 py-2" to="/estoque">
+                <BsBoxSeam /> <span className="">Estoque</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-white" to="/manutencao">
-                <FaTools /> Manutenção
+              <Link className="nav-link text-white d-flex align-items-center gap-1 py-2" to="/manutencao">
+                <FaTools /> <span className="">Manutenção</span>
               </Link>
             </li>
           </ul>
-          <ul className="navbar-nav mr-auto mb-lg-0">
+          <ul className="navbar-nav mb-2 mb-lg-0 flex-row flex-lg-row justify-content-center justify-content-lg-end w-100">
             <li className="nav-item">
-                <Link className="nav-link" id="paginaLogin" to="/login" ><FaUser /></Link>
+              <Link className="nav-link text-white px-2" id="paginaLogin" to="/login">
+                <FaUser />
+              </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" id="loginButton" to="/logout"><IoLogOutOutline id="iconLogin" /></Link>
+              <Link className="nav-link text-white px-2" id="loginButton" to="/logout">
+                <IoLogOutOutline id="iconLogin" />
+              </Link>
             </li>
           </ul>
         </div>
