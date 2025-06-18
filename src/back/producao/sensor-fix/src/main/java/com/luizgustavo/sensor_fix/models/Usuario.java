61db713cba742b1d867cb67cc5123850dc87c172
jjoaom <joaomarcos.aquino@hotmail.com>
2025-06-18 16:03:58 -0300

    add unique to username!

--- src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/models/Usuario.java
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/models/Usuario.java
@@ -9,6 +9,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
     private String password;
 
