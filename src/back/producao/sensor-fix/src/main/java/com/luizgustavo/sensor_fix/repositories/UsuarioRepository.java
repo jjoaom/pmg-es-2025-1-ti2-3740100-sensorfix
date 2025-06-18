2e6ed19fb1bbd5167760db489daca0473d810d5e
jjoaom <joaomarcos.aquino@hotmail.com>
2025-06-18 13:02:10 -0300

    fix: minor issue on UsuarioRepository

--- src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/repositories/UsuarioRepository.java
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/repositories/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.luizgustavo.sensor_fix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.luizgustavo.sensor_fix.models.Usuario;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
