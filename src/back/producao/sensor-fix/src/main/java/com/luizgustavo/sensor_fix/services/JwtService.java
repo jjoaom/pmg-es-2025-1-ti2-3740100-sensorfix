c4e0c3828717ba79aed5c41fd5658e057924c617
jjoaom <joaomarcos.aquino@hotmail.com>
2025-06-22 01:34:58 -0300

    fix: jwtservice

--- src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/services/JwtService.java
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/services/JwtService.java
@@ -1,47 +1,49 @@
 package com.luizgustavo.sensor_fix.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.util.Date;
+import java.security.Key;
+
 import com.luizgustavo.sensor_fix.models.Role;
-import java.security.Key;
-import io.jsonwebtoken.security.Keys;
-import java.security.Key;
 
 @Service
 public class JwtService {
 
-
-    private static final Key KEY;
+    private final Key key;
 
     public JwtService(@Value("${jwt.secret}") String secret) {
-        this.KEY = Keys.hmacShaKeyFor(Base64.getEncoder().encodeToString(secret.getBytes()).getBytes());
+        // chave precisa mínimo de 32 bytes para HS256
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
-
 
     public String generateToken(String username, Role role) {
-    return Jwts.builder()
-            .setSubject(username)
-            .claim("role", role.name())
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hora
-            .signWith(KEY, SignatureAlgorithm.HS256)
-            .compact();
-}
-
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role.name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hora
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public Claims extractClaims(String token) {
-    try {
-        return Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
-    } catch (Exception e) {
-        throw new IllegalStateException("Token inválido ou expirado.");
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            throw new IllegalStateException("Token inválido ou expirado.");
+        }
     }
-}
-
 
     public String extractUsername(String token) {
         return extractClaims(token).getSubject();
