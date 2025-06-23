13147ed32e506640582d4f2b29d56293d9c3695e
jjoaom <joaomarcos.aquino@hotmail.com>
2025-06-23 11:39:10 -0300

    alter: removed vulnerability csrf disabled

--- src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/config/SecurityConfig.java
+++ src/back/producao/sensor-fix/src/main/java/com/luizgustavo/sensor_fix/config/SecurityConfig.java
@@ -50,7 +50,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests()
                 .antMatchers("/auth/**").permitAll() // login, register, etc.
